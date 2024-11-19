18b0b324d7c9331b3532e3fc213fb0e3df74363e
nicolasitpereira <nicolas.pereira.it@gmail.com>
2024-11-18 22:05:56 -0300

    Update script.js

--- mapeamento/assets/javascript/script.js
+++ mapeamento/assets/javascript/script.js
@@ -1,3 +1,7 @@
 document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
+
+document.querySelector("#logout_btn").addEventListener("click", function() {
+    window.location.href = "../LoginCadastro/login.html";
+});
